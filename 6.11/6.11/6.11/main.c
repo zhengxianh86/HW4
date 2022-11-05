996d1ce9249f383a5f401791b92f8213a949ff4c
zhengxianh86 <zhengxianh86@gmail.com>
2022-11-05 09:26:38 +0800

    V1.2

--- 6.11/6.11/6.11/main.c
+++ 6.11/6.11/6.11/main.c
@@ -1,27 +1,35 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-//void bubleSort(int *b[]);
+#define SIZE 5
 
 int main()
 {
 	int i, j, k, tmp;
-	int a[5] = {26,5,81,7,63};
-	for (i = 0; i < 4; i++)
+	int a[SIZE] = { 1254,77,8,774,54 };
+	for (i = 0; i < SIZE-1; i++)
 	{
-
-		for (j = 0; j < 4; j++)
+		for (j = 0, k = 0; j < SIZE-1-i; j++)//a要求是加入-i
 		{
-			if (a[j] > a[j + 1])
+			if (a[j] > a[j + 1])//向上浮一個
 			{
 				tmp = a[j];
 				a[j] = a[j + 1];
 				a[j + 1] = tmp;
 			}
+			else//b要求，都沒有改變則跳出
+			{
+				k++;
+				if (k == SIZE-1-i) goto end;
+			}
 		}
-		printf("Loop %d :",i);
-		for (j = 0; j < 5; j++)
-			printf("%4d",a[j]);
+		printf("Loop %d :",i+1);
+		for (j = 0; j < SIZE; j++)
+			printf("%5d",a[j]);
 		printf("\n");
 	}
+end:
+	printf("\na[] =   ");
+	for (j = 0; j < SIZE; j++)
+		printf("%5d", a[j]);
+	printf("\n");
 }
